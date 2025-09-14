ce2727dad881b35e6379e67a81ca938b783b2774
Khetag <pubgccntgml@gmail.com>
2025-09-14 22:07:41 +0300

    Aside completed And many small fixes

---
+++ vue-project/src/components/mainView/mainView.vue
@@ -0,0 +1,19 @@
+<template>
+    <section class="mainView__section">
+        <div class="container mainView__wrapper">
+            <mainAside />
+            <DashBoardBlock />
+
+        </div>
+    </section>
+</template>
+
+<script setup lang="ts">
+import mainAside from "../mainAside/mainAside.vue";
+import DashBoardBlock from "../DashBoardBlock/DashBoardBlock.vue";
+
+</script>
+
+<style scoped lang="scss">
+@use '@/components/mainView/mainView.scss';
+</style>
---
+++ vue-project/src/views/SavingsView/SavingsView.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="budget__wrapper">
+        <h1>Savings Page</h1>
+        <p>Savings</p>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+.budget__wrapper
+{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    padding: 100px;
+}
+h1
+{
+    font-size: 40px;
+    color: #FFF;
+    margin-bottom: 20px;
+}
+p
+{
+    font-size: 30px;
+    color: #c0c0c0;
+}
+</style>
