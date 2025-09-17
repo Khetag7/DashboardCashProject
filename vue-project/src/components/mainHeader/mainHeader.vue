16e954fcd5317303cd825119aef4a000c31542e0
Khetag <pubgccntgml@gmail.com>
2025-09-17 19:39:31 +0300

    Dashboard header completed

--- vue-project/src/components/mainHeader/mainHeader.vue
+++ vue-project/src/components/mainHeader/mainHeader.vue
@@ -1,6 +1,15 @@
 <template>
     <header class="dashboard__header">
-        <h1>Home</h1>
+        <h1 class="dashboard__header-title">Home</h1>
+        <div class="dashboard__header-block">
+            <img class="dashboard__header-block-search" src="@/assets/images/SearchSvg.svg" alt="">
+
+            <router-link class="dashboard__header-block-link" to="/#">
+                <img class="dashboard__header-block-notifi" src="@/assets/images/NotificationsSvg.svg" alt="">
+                <div class="header__ellipse-orange"></div>
+            </router-link>
+            
+        </div>
     </header>
 </template>
 
